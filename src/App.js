4e16bdf4a7e8a2122f0a2f0546fa03097293aa08
Elzar <elzar@MacBook-Pro-Elzar.local>
2023-11-30 14:22:41 +0600

    start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MiniDrawer from "./components/SideBar";
+
+const App = () => {
+  return (
+    <div>
+      {/* <img src="https://media.istockphoto.com/id/495755752/vector/kyrgyzstan-black-and-white-version-flag-rectangular-shape-icon.jpg?s=170667a&w=0&k=20&c=Do0ECtw28GUHipy-q5fwg6Yk9_drk-g3E3x2g_gvCTk=" /> */}
+      <MiniDrawer />
+    </div>
+  );
+};
+
+export default App;
