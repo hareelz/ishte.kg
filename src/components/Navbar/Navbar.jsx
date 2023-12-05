6bd911e8c5b79c4a0560b4f49a6983ec46abe6a2
Elzar <elzar@MacBook-Pro-Elzar.local>
2023-12-05 17:13:39 +0600

    nav

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,195 +1,246 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
-import ForumOutlinedIcon from "@mui/icons-material/ForumOutlined";
+// import * as React from "react";
+// import AppBar from "@mui/material/AppBar";
+// import Box from "@mui/material/Box";
+// import Toolbar from "@mui/material/Toolbar";
+// import IconButton from "@mui/material/IconButton";
+// import Typography from "@mui/material/Typography";
+// import Menu from "@mui/material/Menu";
+// import MenuIcon from "@mui/icons-material/Menu";
+// import Container from "@mui/material/Container";
+// import Avatar from "@mui/material/Avatar";
+// import Button from "@mui/material/Button";
+// import Tooltip from "@mui/material/Tooltip";
+// import MenuItem from "@mui/material/MenuItem";
+// import AdbIcon from "@mui/icons-material/Adb";
+// import ForumOutlinedIcon from "@mui/icons-material/ForumOutlined";
 
-import "../Navbar/Navbar.css";
-import { Link } from "react-router-dom";
+// import "../Navbar/Navbar.css";
+// import { Link } from "react-router-dom";
 
-const pages = [
-  "Главная Страница",
-  "Как это работает?",
-  "Поиск работы",
-  "Посты",
-  <ForumOutlinedIcon />,
-  "Войти",
-];
-const settings = ["Register", "Login", "Logout"];
-const paths = ["/", "/info", "filter", "add-posts", "/auth"];
+// const pages = [
+//   "Главная Страница",
+//   "Как это работает?",
+//   "Поиск работы",
+//   "Посты",
+//   <ForumOutlinedIcon />,
+//   "Войти",
+// ];
+// const settings = ["Register", "Login", "Logout"];
+// const paths = ["/", "/info", "filter", "add-posts", "/auth"];
 
-function Navbar() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+// function Navbar() {
+//   const [anchorElNav, setAnchorElNav] = React.useState(null);
+//   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+//   const handleOpenNavMenu = (event) => {
+//     setAnchorElNav(event.currentTarget);
+//   };
+//   const handleOpenUserMenu = (event) => {
+//     setAnchorElUser(event.currentTarget);
+//   };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+//   const handleCloseNavMenu = () => {
+//     setAnchorElNav(null);
+//   };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+//   const handleCloseUserMenu = () => {
+//     setAnchorElUser(null);
+//   };
 
+//   return (
+//     <AppBar
+//       className="navbar"
+//       position="static"
+//       sx={{ position: "fixed", zIndex: "2", background: "black" }}
+//     >
+//       <Container maxWidth="xl">
+//         <Toolbar disableGutters>
+//           <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+//           <Typography
+//             variant="h6"
+//             noWrap
+//             component="a"
+//             href="#app-bar-with-responsive-menu"
+//             sx={{
+//               mr: 2,
+//               display: { xs: "none", md: "flex" },
+//               fontFamily: "monospace",
+//               fontWeight: 700,
+//               letterSpacing: ".3rem",
+//               color: "inherit",
+//               textDecoration: "none",
+//             }}
+//           >
+//             LOGO
+//           </Typography>
+
+//           <Box
+//             sx={{
+//               background: "black",
+//               flexGrow: 1,
+//               display: { xs: "flex", md: "none" },
+//             }}
+//           >
+//             <IconButton
+//               size="large"
+//               aria-label="account of current user"
+//               aria-controls="menu-appbar"
+//               aria-haspopup="true"
+//               onClick={handleOpenNavMenu}
+//               color="inherit"
+//             >
+//               <MenuIcon />
+//             </IconButton>
+//             <Menu
+//               id="menu-appbar"
+//               anchorEl={anchorElNav}
+//               anchorOrigin={{
+//                 vertical: "bottom",
+//                 horizontal: "left",
+//               }}
+//               keepMounted
+//               transformOrigin={{
+//                 vertical: "top",
+//                 horizontal: "left",
+//               }}
+//               open={Boolean(anchorElNav)}
+//               onClose={handleCloseNavMenu}
+//               sx={{
+//                 display: { xs: "block", md: "none" },
+//               }}
+//             >
+//               {pages.map((page) => (
+//                 <MenuItem key={page} onClick={handleCloseNavMenu}>
+//                   <Typography textAlign="center">{page}</Typography>
+//                 </MenuItem>
+//               ))}
+//             </Menu>
+//           </Box>
+//           <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+//           <Typography
+//             variant="h5"
+//             noWrap
+//             component="a"
+//             href="#app-bar-with-responsive-menu"
+//             sx={{
+//               mr: 2,
+//               display: { xs: "flex", md: "none" },
+//               flexGrow: 1,
+//               fontFamily: "monospace",
+//               fontWeight: 700,
+//               letterSpacing: ".3rem",
+
+//               textDecoration: "none",
+//             }}
+//           >
+//             LOGO
+//           </Typography>
+//           <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+//             {pages.map((page, index) => (
+//               <Link to={paths[index]} className="navbar_item">
+//                 <Button
+//                   key={page}
+//                   onClick={handleCloseNavMenu}
+//                   sx={{
+//                     height: "5em",
+//                     my: 2,
+//                     color: "#fff",
+//                     background: "none",
+//                     margin: "auto 0",
+//                     display: "block",
+//                     marginLeft: "5em",
+//                     fontSize: ".7em",
+//                   }}
+//                 >
+//                   {page}
+//                 </Button>
+//               </Link>
+//             ))}
+//           </Box>
+
+//           <Box sx={{ flexGrow: 0 }}>
+//             <Tooltip title="">
+//               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+//                 <Avatar alt="" src="/static/images/avatar/2.jpg" />
+//               </IconButton>
+//             </Tooltip>
+//             {/* <Menu
+//               sx={{ mt: "45px" }}
+//               id="menu-appbar"
+//               anchorEl={anchorElUser}
+//               anchorOrigin={{
+//                 vertical: "top",
+//                 horizontal: "right",
+//               }}
+//               keepMounted
+//               transformOrigin={{
+//                 vertical: "top",
+//                 horizontal: "right",
+//               }}
+//               open={Boolean(anchorElUser)}
+//               onClose={handleCloseUserMenu}
+//             >
+//               {settings.map((setting) => (
+//                 <MenuItem key={setting} onClick={handleCloseUserMenu}>
+//                   <Typography textAlign="center">{setting}</Typography>
+//                 </MenuItem>
+//               ))}
+//             </Menu> */}
+//           </Box>
+//         </Toolbar>
+//       </Container>
+//     </AppBar>
+//   );
+// }
+// export default Navbar;
+
+import React, { useDebugValue, useEffect } from "react";
+import "./Navbar.css";
+import { Link, useNavigate } from "react-router-dom";
+
+import { Avatar, IconButton, Tooltip } from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
+const Navbar = () => {
+  const navigate = useNavigate();
   return (
-    <AppBar
-      className="navbar"
-      position="static"
-      sx={{ position: "fixed", zIndex: "2", background: "black" }}
-    >
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography>
+    <div className="nav-bcg">
+      <a href="/" className="logo-home">
+        <img
+          src="https://www.f-cdn.com/assets/main/en/assets/freelancer-logo-light.svg"
+          alt=""
+          className="navbar-logo"
+        />
+      </a>
 
-          <Box
-            sx={{
-              background: "black",
-              flexGrow: 1,
-              display: { xs: "flex", md: "none" },
-            }}
-          >
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page, index) => (
-              <Link to={paths[index]} className="navbar_item">
-                <Button
-                  key={page}
-                  onClick={handleCloseNavMenu}
-                  sx={{
-                    height: "5em",
-                    my: 2,
-                    color: "#fff",
-                    background: "none",
-                    margin: "auto 0",
-                    display: "block",
-                    marginLeft: "5em",
-                    fontSize: ".7em",
-                  }}
-                >
-                  {page}
-                </Button>
-              </Link>
-            ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            {/* <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu> */}
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+      <div className="">
+        <input type="text" />
+        <button>найти</button>
+      </div>
+      <ul className="navbar_list">
+        <li className="nav-list-item">
+          <Link to={"/roster"} className="link-nav">
+            Roster
+          </Link>
+        </li>
+        <li className="nav-list-item">
+          <Link to={"/cardPage"} className="link-nav">
+            Buy Games
+          </Link>
+        </li>
+        <li className="nav-list-item">
+          <Link to={"/admin"} className="link-nav">
+            Admin
+          </Link>
+        </li>
+        <li className="nav-list-item">
+          <Link to={"/cartPage"} className="link-nav">
+            Cart
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
-}
+};
+
 export default Navbar;
