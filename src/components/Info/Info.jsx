50e5f3f0e8476b15233d85f8333e8a83ee8c0a2f
Сыймык Джумагулов <jumagulovsyimyk0404@gmail.com>
2023-12-05 17:22:23 +0600

    .

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -45,10 +45,8 @@ const Info = () => {
               </li>
               <li className="block-desc_top__desc-list-item">
                 <h3>Выберите фрилансеров</h3>
-                Не бывает слишком большой или слишком мелкой работы. У нас есть
-                фрилансеры с более чем 1800 навыками для работы любого объема и
-                с любым бюджетом. Для нас нет слишком сложной работы. Мы
-                справимся с любой!
+                Не бывает слишком большой или слишком мелкой работы. Для нас нет
+                слишком сложной работы. Мы справимся с любой!
               </li>
               <li className="block-desc_top__desc-list-item">
                 <h3>Платите безопасно</h3>
@@ -60,7 +58,54 @@ const Info = () => {
           </div>
           <span className="line2"></span>
 
-          <div className="block-desc__bottom"></div>
+          <div className="block-desc__bottom">
+            <h2 className="block-desc_bottom__title">Каковы преимущества?</h2>
+            <ul className="block-desc_bottom__desc-list">
+              <li className="block-desc_bottom__desc-list-item">
+                <h3>Просматривайте портфолио</h3>
+                Находите профессионалов, которым вы можете доверять,
+                просматривая образцы их предыдущей работы и отзывы в их профиле.
+              </li>
+              <li className="block-desc_bottom__desc-list-item">
+                <h3>Быстрые заявки</h3>
+                Быстро получайте ценовые предложения от наших талантливых
+                фрилансеров без каких-либо обязательств с вашей стороны. 80%
+                проектов принимаются в течение 60 секунд.
+              </li>
+              <li className="block-desc_bottom__desc-list-item">
+                <h3>Отслеживайте прогресс</h3>
+                Где бы вы ни находились, оставайтесь в курсе событий. Всегда
+                знайте, чем занимаются ваши фрилансеры.
+              </li>
+            </ul>
+          </div>
+          <span className="line3"></span>
+        </div>
+
+        <div className="block_pictures">
+          <h3 className="block_pictures__title">
+            Реализуйте свои идеи вместе с нами
+          </h3>
+          <div className="block_picture_1">
+            <img
+              className="picture_laptop"
+              src="https://www.f-cdn.com/assets/main/en/assets/home/crowd-favorites/v2/seo-expert-v2.jpg"
+              alt=""
+            />
+            <img
+              className="picture_tablet"
+              src="https://www.f-cdn.com/assets/main/en/assets/home/crowd-favorites/v2/shopify-website-v2.jpg"
+              alt=""
+            />
+          </div>
+          <div className="block_picture_2">
+            <img
+              className="picture_book"
+              src="https://www.f-cdn.com/assets/main/en/assets/home/crowd-favorites/v2/book-cover-design-v2.jpg"
+              alt=""
+            />
+          </div>
+          <div className="block_picture_3"></div>
         </div>
 
         <video
